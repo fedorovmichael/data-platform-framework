bf30960e803986f71342b2ea56f62e4148225234
Michael Fedorov <michaelf@realcommerce.co.il>
2026-07-21 19:54:42 +0300

    added csv spark source

--- app/sources/csv_spark_source.py
+++ app/sources/csv_spark_source.py
@@ -2,7 +2,7 @@ from app.sources.source_base import Source
 from pyspark.sql import SparkSession, DataFrame
 
 
-class CsvSparkSource(Source):
+class CsvSparkSource(Source[DataFrame]):
 
     def read(self, spark: SparkSession, csv_path: str) -> DataFrame:
         if spark is None:
